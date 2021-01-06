f801e13c991504dd9279fecfcf169eb0b20275d4
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2021-01-06 23:26:48 +0100

    Implemented basic functionality and a simple test

--- src/ThermodynamicIntegration.jl
+++ src/ThermodynamicIntegration.jl
@@ -1,5 +1,49 @@
 module ThermodynamicIntegration
 
-# Write your package code here.
+using AdvancedHMC
+using Requires
+using Trapz
+using Statistics
+using ForwardDiff
+using ProgressMeter
+
+export ThermInt
+
+struct ThermInt{V}
+    schedule::V
+    n_samples::Int
+    n_warmup::Int
+end
+
+function ThermInt(schedule; n_samples::Int=2000, n_warmup::Int=500)
+    ThermInt(schedule, n_samples, n_warmup)
+end
+
+function ThermInt(n_steps::Int=30; n_samples::Int=2000, n_warmup::Int=500)
+    ThermInt(((1:n_steps) ./ n_steps).^5, n_samples, n_warmup)
+end
+
+function (alg::ThermInt)(loglikelihood, logprior, θ::AbstractVector)
+    ΔlogZ = @showprogress [evaluate_loglikelihood(loglikelihood, logprior, alg, θ, β) for β in alg.schedule]
+    return trapz(alg.schedule, ΔlogZ)
+end
+
+function evaluate_loglikelihood(loglikelihood, logprior, alg::ThermInt, θ, β::Real)
+    logπ(θ) = β * loglikelihood(θ) + logprior(θ)
+
+    D = length(θ)
+    metric = DiagEuclideanMetric(D)
+    hamiltonian = Hamiltonian(metric, logπ, ForwardDiff)
+
+    initial_ϵ = find_good_stepsize(hamiltonian, θ)
+    integrator = Leapfrog(initial_ϵ)
+
+    proposal = NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator)
+    adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.8, integrator))
+
+    samples, stats = sample(hamiltonian, proposal, θ, alg.n_samples, adaptor, alg.n_warmup; verbose=false, progress=false)
+    θ .= samples[end]
+    return mean(loglikelihood, samples) 
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using ThermodynamicIntegration
+using Distributions
 using Test
+using ForwardDiff
+# @testset "ThermodynamicIntegration.jl" begin
+D = 5
+prior = MvNormal(0.5 * ones(D))
+likelihood = MvNormal(2.0 * ones(D))
+logprior(x) = logpdf(prior, x)
+loglikelihood(x) = logpdf(likelihood, x)
+alg = ThermInt(n_samples=5000)
+logZ = alg(logprior, loglikelihood, rand(prior))
 
-@testset "ThermodynamicIntegration.jl" begin
-    # Write your tests here.
-end
+Σ = Diagonal(inv(inv(cov(prior)) + inv(cov(likelihood))))
+posterior = MvNormal(Diagonal(Σ))
+0.5 * (logdetcov(posterior) - D * log(2π))
+# end
