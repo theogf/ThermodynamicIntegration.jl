56992daf82b363ce4f4376fc0b5223fa8a5bc160
Documenter.jl <documenter@juliadocs.github.io>
2025-06-05 01:08:33 +0000

    build based on 309c05a

---
+++ previews/PR44/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ThermodynamicIntegration","page":"Home","title":"ThermodynamicIntegration","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#ThermodynamicIntegration.ThermInt","page":"Home","title":"ThermodynamicIntegration.ThermInt","text":"ThermInt([rng::AbstractRNG]; n_steps=30, n_samples=2000, n_warmup=500)\nThermInt([rng::AbstractRNG], schedule; n_samples=2000, n_warmup=500)\n\nschedule can be any iterable object\nn_steps is the number of steps for the schedule using the formula\n\n(1:n_steps) ./ n_steps).^5\n\nA ThermInt object can then be used as a function:\n\n    alg = ThermInt(30)\n    alg(loglikelihood, logprior, x_init)\n\n\n\n\n\n","category":"type"}]
+}
